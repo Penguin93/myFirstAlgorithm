e406d1cdc3c3de2cca5a9dc0d72e7b18c02df7c2
Penguin93 <marcus.parry2@education.nsw.gov.au>
2026-02-16 10:19:22 +1100

    Adjusted Discount Procedure
    Criteria for discount is now greater than or equal to 10

--- main.py
+++ main.py
@@ -4,18 +4,20 @@
 #print (totalcost)
 
 #------------------------------------------------
-
+#calculateTotal
 def add(foodprice1,foodprice2):
     answer=foodprice1+foodprice2
     return answer
 
-result=add(15,13)
+result=add(3,7)
 print(result)
 
 #------------------------------------------------
-
-if result>10:
+#applyDiscount
+if result>=10:
     answer=result*0.9
 else:
     answer=result
 print(answer)
+
+#------------------------------------------------
