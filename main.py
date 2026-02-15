d4aec0cacdfa4a9825fbf6c65d9c671614ddac88
Penguin93 <marcus.parry2@education.nsw.gov.au>
2026-02-16 10:01:37 +1100

    Create main.py
    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#foodprice1=7
+#foodprice2=10
+#totalcost=foodprice1+foodprice2
+#print (totalcost)
+
+#------------------------------------------------
+
+def add(foodprice1,foodprice2):
+    answer=foodprice1+foodprice2
+    return answer
+
+result=add(17,13)
+print(result)
+
+#------------------------------------------------
